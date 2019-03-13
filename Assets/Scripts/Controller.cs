4e17acd4476f762014843d488e183618f4f417ba
newbrine <35578361+newbrine@users.noreply.github.com>
2019-03-12 22:00:12 -0500

    d

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; //Add this for NavMeshAgent
+
+public class Controller : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent meshAgent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) //Pressed primary button
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                meshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
